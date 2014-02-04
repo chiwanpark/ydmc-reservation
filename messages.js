f751c714ff1e22b2e0747e2da3fdf19a2b512b37
Chiwan Park <chiwanpark@icloud.com>
2014-02-05 04:07:52 +0900

    Add i18n messages module

---
+++ messages.js
@@ -0,0 +1,10 @@
+module.exports = {
+  duplicatedEmail: '선생님 이메일이 이미 존재합니다.',
+  errorOnDatabase: '데이터베이스 접근에 실패했습니다.',
+  inputValidationFailed: '입력된 데이터가 올바르지 않습니다.',
+  unauthorized: '권한이 없습니다.',
+  loginFailed: '로그인에 실패하였습니다.',
+  loginSuccess: '로그인에 성공했습니다.',
+  logoutSuccess: '로그아웃에 성공했습니다.',
+  updateSuccess: '정보 수정에 성공했습니다.'
+};
