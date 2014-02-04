a62fa68779096f7df20d00e6c67377c116f32e5a
Chiwan Park <chiwanpark@icloud.com>
2014-02-05 02:38:33 +0900

    Add hash method into Util module

--- util.js
+++ util.js
@@ -1,9 +1,26 @@
 'use strict';
 
+var _ = require('lodash');
+var Crypto = require('crypto');
+
 var Util = {};
 
 Util.capitalize = function (string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
+
+Util.hash = function (args) {
+  var hasher = Crypto.createHash('sha1');
+
+  if (_.isArray(args)) {
+    _.forEach(args, function(value) {
+      hasher.update(value);
+    });
+  } else {
+    hasher.update(args);
+  }
+
+  return hasher.digest('base64');
 };
 
 module.exports = Util;
