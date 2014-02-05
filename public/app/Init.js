9b3d929e09ae644df49c212210e0de16d969b765
Chiwan Park <chiwanpark@icloud.com>
2014-02-06 03:19:47 +0900

    Modify Code indentation

--- public/app/Init.js
+++ public/app/Init.js
@@ -12,18 +12,13 @@ Ext.Loader.setConfig({
 Ext.application({
   name: 'YdmcReservation',
   appFolder: '/app',
-  requires: [
-    'YdmcReservation.view.Login'
-  ],
-  views: [
-    'Login'
-  ],
+  requires: ['YdmcReservation.view.Login'],
+  views: ['Login'],
   controllers: [
     'YdmcReservation.controller.Auth'
   ],
   launch: function () {
     var loginView = Ext.create('YdmcReservation.view.Login');
-
     loginView.center().show();
   }
 });
